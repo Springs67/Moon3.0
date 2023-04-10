35ee07f668e85497658a976c6c1aae686d10fba3
Spring67#0001 <109059103+Springs67@users.noreply.github.com>
2023-04-10 13:04:17 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 6872265039 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/Moon3.0/3kdIKfmsdSf/bedwars/6872265039.lua"))()
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Springs67/Moon3.0/3kdIKfmsdSf/bedwars/6872274481.lua"))()
+end
